8601f09362883b6caec6cf14f5555fbbacd066a9
Tubhyamjain <142088254+Tubhyamjain@users.noreply.github.com>
2023-08-16 13:32:11 +0530

    Update Scipt.js

--- Scipt.js
+++ Scipt.js
@@ -11,3 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+// Inside your scripts.js file or within <script> tags in your HTML
+document.addEventListener("DOMContentLoaded", function () {
+    const optionsToggle = document.querySelectorAll(".options-toggle");
+
+    optionsToggle.forEach(function (toggle) {
+        const icon = toggle.querySelector("i");
+        const options = toggle.querySelector(".options");
+
+        icon.addEventListener("click", function () {
+            options.classList.toggle("show-options");
+        });
+    });
+});
